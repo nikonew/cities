23b328ea1142e0479b2ffa8d741da39d81b5c287
nikonew <spiritofadv@yandex.ru>
2024-12-03 22:42:07 +0300

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/app/app';
+import { AuthorizationStatus } from './app/router/router/router';
+import { Provider } from 'react-redux';
+import {store} from './store';
+import browserHistory from './browser-history';
+import HistoryRouter from './componets/history-router/history-router';
+
+
+const authorizationStatus = AuthorizationStatus.Auth;
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <HistoryRouter history={browserHistory} basename={import.meta.env.BASE_URL}>
+        <App authorizationStatus= {authorizationStatus}/>
+      </HistoryRouter>
+    </Provider>
+  </React.StrictMode>
+);
